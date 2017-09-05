24c11ff84fd496688a222fc615760b4210d3e08c
Minh <minhnguyen1995@googlemail.com>
2017-09-05 20:19:21 +0100

    .

--- PureState.scala
+++ PureState.scala
@@ -1,9 +1,31 @@
+
 trait RNG {
+    // Essentially a cyclic definition of RNG/State //
+    type Rand[+A] = RNG => (A, RNG)
+    
     def nextInt: (Int, RNG);
+    
+}
+
+object folds{
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B) : B = l match {
+    case Nil => z
+    case ::(x, xs) => foldLeft(xs, f (z, x)) (f)
+  }
+  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = l match {
+    case Nil => z
+    case ::(x, xs) =>  f(x, foldRight(xs, z)(f))
+  }
 }
 
 object RNG {
+    import folds._
+
+    type Rand[+A] = RNG => (A, RNG)
     def simple(seed: Long): RNG = new RNG {
+        /**
+        This allows us to take our state s to (a, s). 
+        / */
         def nextInt = {
             val seed2 = (seed*0x5DEECE66DL + 0xBL) &
                         ((1L << 48) - 1)
@@ -11,9 +33,7 @@ object RNG {
             simple(seed2))      // Here our state is our value wrapped up in 
         }                       // a function 'simple'.
     }                           
-    /**
-    This allows us to take our returned state s = 
-    / */
+    
     def positiveInt(rng: RNG): (Int, RNG) = {
         val (a, s) = rng.nextInt 
         (a.abs, s)
@@ -35,6 +55,59 @@ object RNG {
             val (as, s2) = ints(count - 1)(s1)
             (::(a1, as), s2)
         }
+    }
+    
+    // return: a => (s => (a, s))   //
+    def unit[A](a: A): Rand[A] = 
+        rng => (a, rng)
+    // map: When given s, runState on s to give (a, s), then map f on value a
+    def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+        rng => {
+            val (a, rng2) = s(rng)
+            (f(a), rng2)
+        }
+    val int: Rand[Int] = _.nextInt
+    // Defining state processors (s => (a, s))
+    def positiveMax(n: Int): Rand[Int] = {
+        rng: RNG => {
+            val (a, rng2) = rng.nextInt
+            (a/(a-n).abs, rng2)
+        }
+    }
+    def doubleM: Rand[Double] = 
+        map(rng => rng.nextInt)(a => a.asInstanceOf[Double])
+    
+    def map2[A, B, C](s1: Rand[A], s2: Rand[B])(f: (A, B) => C): Rand[C] = {
+        rng => {
+            val (a, st1) = s1(rng)
+            val (b, st2) = s2(st1)
+            (f(a,b), st2)
+        }
+    }
+    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+        foldRight(fs, unit(List[A]())) ((nxt, acc) => {
+            state: RNG => {
+                val (a1, s1) = acc(state)
+                val (a2, s2) = nxt(s1)
+                (::(a2, a1), s2)
+            }
+        }) 
+    }
+    def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+        rng => { 
+            val (a, s) = f(rng)
+            g(a)(s)
+        }
+    }
+    def posInt: Rand[Int] = {
+        flatMap(s1 => s1.nextInt)((a: Int) => {
+            if (a != Int.MinValue) {
+                s => (a.abs, s)
+            }
+            else {
+                s => s.nextInt
+            }
+        })
     }
 }
 
